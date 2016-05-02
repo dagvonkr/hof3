ab7d836f73215705fb807477014b991465a0be2d
Sebastian Sastre <sebastian@flowingconcept.com>
2016-05-02 15:44:16 -0300

    adds accounts-password

--- server/publish/users.js
+++ server/publish/users.js
@@ -1,13 +1,11 @@
-// import { Meteor } from 'meteor/meteor';
+Meteor.publish('users', function () {
+  return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
+});
 
-// Meteor.publish('users', function () {
-//   return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
-// });
-
-// Meteor.startup(function () {
-//   if ((Meteor.users.find().count() === 0) && Meteor.settings.initial) {
-//     Accounts.createUser({
-//       // ...Meteor.settings.initial.user
-//     });
-//   }
-// });
+Meteor.startup(function () {
+  if ((Meteor.users.find().count() === 0) && Meteor.settings.initial) {
+    Accounts.createUser({
+      // ...Meteor.settings.initial.user
+    });
+  }
+});
