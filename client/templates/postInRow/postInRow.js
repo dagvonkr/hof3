b2cbd20ba2c2d6bdb8e6964efdefe8e26a589628
Sebastian Sastre <sebastian@flowingconcept.com>
2016-05-15 18:09:49 -0300

    wip sets css attributes (no reactivity)

--- client/templates/postInRow/postInRow.js
+++ client/templates/postInRow/postInRow.js
@@ -4,14 +4,24 @@ import Posts from '../../../model/posts';
 let tpl = Template.postInRow;
 
 tpl.onCreated(function() {
-  var self = this;
+  let self = this;
 
   self.ready = new ReactiveVar();
-  self.autorun(function () {
-    var postId = self.data._id;
-    var handle = subs.subscribe('post', postId);
+  self.autorun(() => {
+    const postId = self.data._id;
+    const handle = subs.subscribe('post', postId);
     self.ready.set(handle.ready());
+    if(self.ready.get()) {
+      Meteor.setTimeout(function () {
+        updatePostStyleOn(self);
+      }, Meteor.settings.public.postInRowOnReadyDelay);
+    }
   });
+});
+
+tpl.onRendered(function () {
+  let self = this;
+
 });
 
 tpl.helpers({
@@ -23,6 +33,28 @@ tpl.helpers({
     return Template.instance().ready.get();
   }
 });
+
+function updatePostStyleOn (aTemplate) {
+  // Updates the style of the post on aTemplate.
+  let title = aTemplate.find('[name="title"]');
+  const customStyle = getPostStyle(aTemplate.data);
+  $(title).css(customStyle);
+}
+
+function getPostStyle (aPost) {
+    // Answers the custom style of this post.
+
+    const customStyle = {
+      top: (!!aPost.style && !!aPost.style.header && !!aPost.style.header.top)?`${aPost.style.header.top}px`:'50px',
+      left: (!!aPost.style && !!aPost.style.header && !!aPost.style.header.left)?`${aPost.style.header.left}px`:'0px',
+      width: (!!aPost.style && !!aPost.style.header && !!aPost.style.header.width)?`${aPost.style.header.width}px`:'400px',
+      'font-size': (!!aPost.style && !!aPost.style.header && !!aPost.style.header.fontSize)?`${aPost.style.header.fontSize}px`:'50px',
+      color: (!!aPost.style && !!aPost.style.header && !!aPost.style.header.color)?aPost.style.header.color:'#2d2c2c',
+      'text-shadow': (!!aPost.style && !!aPost.style.header && !!aPost.style.header.textShadow)?aPost.style.header.textShadow:'none',
+      'text-align': (!!aPost.style && !!aPost.style.header && !!aPost.style.header.textAlign)?aPost.style.header.textAlign:'center'
+    };
+    return customStyle;
+}
 
 function hasImageOn (aPost) {
   // Answers true if there are images in aPost.
