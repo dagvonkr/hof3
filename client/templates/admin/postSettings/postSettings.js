4441bc7f55e750576893b4b1dee033675423388a
Sebastian Sastre <sebastian@flowingconcept.com>
2016-05-15 12:02:38 -0300

    attributes with correct initial state

--- client/templates/admin/postSettings/postSettings.js
+++ client/templates/admin/postSettings/postSettings.js
@@ -1,11 +1,64 @@
 let tpl = Template.postSettings;
 
 tpl.onCreated( function () {
+  let self = this;
+
+  self.isPublished = new ReactiveVar( self.data.isPublished || null );
+  self.headerTop = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.top:null );
+  self.headerLeft = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.left:null );
+  self.headerWidth = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.width:null );
+  self.headerFontSize = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.fontSize:null );
+  self.headerTextAlign = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.textAlign:null );
+  self.headerTextShadow = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.textShadow:null );
+  self.headerColor = new ReactiveVar( (!!self.data.style && !!self.data.style.header)?self.data.style.header.color:null );
+});
+
+tpl.onRendered(function () {
+  let self = this;
+
+  $(self.find('#postIsPublished')).prop('checked', self.isPublished.get() );
+  $(self.find('#postHeaderTop')).val( self.headerTop.get() );
+  $(self.find('#postHeaderLeft')).val( self.headerLeft.get() );
+  $(self.find('#postHeaderWidth')).val( self.headerWidth.get() );
+  $(self.find('#postHeaderFontSize')).val( self.headerFontSize.get() );
+  $(self.find('#postHeaderTextAlign')).val( self.headerTextAlign.get() );
+  $(self.find('#postHeaderTextShadow')).val( self.headerTextShadow.get() );
+  $(self.find('#postHeaderColor')).val( self.headerColor.get() );
 
 });
 
 tpl.helpers({
+  isPublished() {
+    return Template.instance().isPublished.get();
+  },
 
+  headerTop() {
+    return Template.instance().headerTop.get();
+  },
+
+  headerLeft() {
+    return Template.instance().headerLeft.get();
+  },
+
+  headerWidth() {
+    return Template.instance().headerWidth.get();
+  },
+
+  headerFontSize() {
+    return Template.instance().headerFontSize.get();
+  },
+
+  headerTextAlign() {
+    return Template.instance().headerTextAlign.get();
+  },
+
+  headerTextShadow() {
+    return Template.instance().headerTextShadow.get();
+  },
+
+  headerColor() {
+    return Template.instance().headerColor.get();
+  }
 });
 
 tpl.events({
@@ -70,7 +123,6 @@ tpl.events({
       whichPost,
       'color',
       $(template.find('#postHeaderColor')).val());
-    // console.log(`postHeaderColor ${$(template.find('select')).val()}`);
   }
 
 });
