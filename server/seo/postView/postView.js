27eac051a09b1c9970e489d94420a8540606a6fa
Sebastian Sastre <sebastian@flowingconcept.com>
2016-07-13 10:11:40 -0300

    adds html to text

--- server/seo/postView/postView.js
+++ server/seo/postView/postView.js
@@ -1,3 +1,5 @@
+import htmlToText from 'html-to-text';
+
 SSR.compileTemplate('seoPostView', Assets.getText('postView.html'));
 
 Template.seoPostView.helpers({
@@ -11,7 +13,10 @@ Template.seoPostView.helpers({
     return Posts.getMainImageUrlFor(aPost);
   },
   getExcerptFor (aPost) {
-    // return extractTextFromHTML(aPost.content);
-    return 'Urna vut dictumst enim penatibus lectus vel tortor et dictumst turpis augue adipiscing. Mattis a ridiculus risus elementum elit egestas egestas amet, tempor sagittis nisi mauris? Augue, nunc porttitor, nunc scelerisque nec, elementum dictumst nec. Ut, sit a lectus montes dolor? Ac duis vel tincidunt elementum nec, placerat amet sagittis amet elementum et, egestas tortor odio, ultrices egestas enim? Aenean nisi! Urna eros ut pid risus tempor, ac scelerisque magna etiam quis enim nec ut elementum sit scelerisque habitasse urna? Cursus porta! Hac, aliquet nunc ultrices? Non nec, tempor? Tincidunt amet phasellus et magna urna, cum ut, nisi phasellus a tristique dis ac! Cursus, placerat natoque cursus, adipiscing in dis, porttitor tortor facilisis. Dolor non tempor. Tristique platea purus.';
+    return extractTextFromHTML(aPost.content);
   }
 });
+
+function extractTextFromHTML (someHTML) {
+  return _(htmlToText.fromString(someHTML)).without('\n').join('');
+}
