402437130a0b43685a84f19c4556cb5362989995
Sebastian Sastre <sebastian@flowingconcept.com>
2016-07-13 11:53:19 -0300

    activating seo only for facebook bot

--- server/seo/seoRoutes.js
+++ server/seo/seoRoutes.js
@@ -4,16 +4,17 @@ Meteor.startup(function () {
   // picker definition. Use this to figure out if a crawler is hitting you
   // ----------------------------------------------------------------------------
   var seoPicker = Picker.filter(function (req, res) {
-    // var isCrawler = [];
-    // var string = req.headers['user-agent'];
-    // isCrawler.push(/_escaped_fragment_/.test(req.url));
-    // if(string){
-    //     isCrawler.push(string.indexOf('facebookexternalhit') >= 0);
-    //     isCrawler.push(string.indexOf('Slack') >= 0);
-    //     isCrawler.push(string.indexOf('Twitterbot') >= 0);
-    // }
-    // return isCrawler.indexOf(true) >= 0;
-    return true;
+    var isCrawler = [];
+    var string = req.headers['user-agent'];
+    isCrawler.push(/facebook/.test(req.url));
+    if(string){
+        isCrawler.push(string.indexOf('Facebot') >= 0);
+        isCrawler.push(string.indexOf('facebookexternalhit') >= 0);
+        isCrawler.push(string.indexOf('Slack') >= 0);
+        isCrawler.push(string.indexOf('Twitterbot') >= 0);
+    }
+    // console.log('seoPicker isCrawler',isCrawler);
+    return isCrawler.indexOf(true) >= 0;
   });
 
   // Indexing user pages
